2612e248e716d7554f7ffa3eb11f7ec185b1d8d8
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-14 15:04:04 +0200

    Range class update

--- TestowanieOprogramowania/UT-SP-UnitTestsExcercises/src/main/java/Range.java
+++ TestowanieOprogramowania/UT-SP-UnitTestsExcercises/src/main/java/Range.java
@@ -3,6 +3,9 @@ public class Range {
     private final long upperBound;
 
     public Range(long lowerBound, long upperBound) {
+        if(lowerBound > upperBound){
+            throw new IllegalArgumentException("lowerBound is bigger than upperBound");
+        }
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
     }
