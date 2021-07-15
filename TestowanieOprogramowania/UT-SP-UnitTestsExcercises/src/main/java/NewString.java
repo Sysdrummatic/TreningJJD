84188008930f0ca4b42e4a109519aa631e4a5fdd
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-15 21:19:32 +0200

    New class NewString.java created for exercise preparation.

---
+++ TestowanieOprogramowania/UT-SP-UnitTestsExcercises/src/main/java/NewString.java
@@ -0,0 +1,20 @@
+public class NewString {
+    public static void main(String[] args) {
+
+        String someLiteral = "some constant value";
+
+        System.out.println("Some" + " " + "string" + " " + "literal.");
+
+        int x = 10;
+        Object y = new Object();
+        System.out.println("Some" + " " + x + " " + "literal" + " " + y);
+
+        System.out.println(1 + 2 + "test");
+        System.out.println("test" + 1 + 2);
+
+        String text1 = "Some";
+        String text2 = " literal.";
+        System.out.println(text1.concat(text2));
+
+    }
+}
