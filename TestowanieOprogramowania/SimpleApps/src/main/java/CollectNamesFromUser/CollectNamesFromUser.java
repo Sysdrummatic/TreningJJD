1ba7bb681c54f52a0570da4302abfa306616b373
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-18 00:30:22 +0200

    CollectNamesFromUser update 1

--- TestowanieOprogramowania/SimpleApps/src/main/java/CollectNamesFromUser/CollectNamesFromUser.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/CollectNamesFromUser/CollectNamesFromUser.java
@@ -1,4 +1,32 @@
 package CollectNamesFromUser;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CollectNamesFromUser {
+
+    public static final String EndOfNames = "-";
+
+    public static void main(String[] args) {
+
+        String newUserName;
+        List<String> UserNameList = new LinkedList<>();
+        Scanner UserName = new Scanner(System.in);
+        do{
+
+            System.out.println("Sign new user's name:");
+            newUserName = UserName.nextLine();
+        if(!newUserName.equals(EndOfNames))
+        {
+            UserNameList.add(newUserName);
+        }
+
+
+        }while(!newUserName.equals(EndOfNames));
+        System.out.println(UserNameList.get(0));
+
+
+
+    }
+
 }
