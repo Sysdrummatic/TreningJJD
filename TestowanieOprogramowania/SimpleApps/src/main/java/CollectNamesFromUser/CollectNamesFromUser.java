f0cba92e82217cb8c5a469555ac4efb961791fb1
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-18 18:00:44 +0200

    CollectNamesFromUser update.

--- TestowanieOprogramowania/SimpleApps/src/main/java/CollectNamesFromUser/CollectNamesFromUser.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/CollectNamesFromUser/CollectNamesFromUser.java
@@ -1,29 +1,25 @@
 package CollectNamesFromUser;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class CollectNamesFromUser {
 
     public static final String EndOfNames = "-";
 
     public static void main(String[] args) {
-
         String newUserName;
-        List<String> UserNameList = new LinkedList<>();
+        Set<String> UserNameList = new HashSet<>();
         Scanner UserName = new Scanner(System.in);
         do{
-
             System.out.println("Sign new user's name:");
             newUserName = UserName.nextLine();
-        if(!newUserName.equals(EndOfNames))
-        {
-            UserNameList.add(newUserName);
-        }
-
+            if(!newUserName.equals(EndOfNames))
+            {
+                UserNameList.add(newUserName);
+            }
 
         }while(!newUserName.equals(EndOfNames));
-        System.out.println(UserNameList.size());
+
+        System.out.println("Wprowadziłeś " + UserNameList.size() + "unikalnych imion.");
 
 
 
