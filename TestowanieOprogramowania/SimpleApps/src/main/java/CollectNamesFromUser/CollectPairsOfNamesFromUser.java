259f714c6b1487e673decdb499912c25bb3c90f5
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-18 21:17:31 +0200

    CollectPairsOfNamesFromUser creation

---
+++ TestowanieOprogramowania/SimpleApps/src/main/java/CollectNamesFromUser/CollectPairsOfNamesFromUser.java
@@ -0,0 +1,32 @@
+package CollectNamesFromUser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CollectPairsOfNamesFromUser {
+
+    public static final String END_OF_NAMES = "-";
+
+    public static void main(String[] args) {
+
+        String newUserName1;
+        String newUserName2;
+        Map<String, String> pairOfUsersName = new HashMap<>();
+        Scanner UserName = new Scanner(System.in);
+
+        do {
+            System.out.println("Type first user name:");
+            newUserName1 = UserName.nextLine();
+            System.out.println("Type second user name:");
+            newUserName2 = UserName.nextLine();
+            if(!newUserName1.equals(END_OF_NAMES))
+            {
+                pairOfUsersName.put(newUserName1, newUserName2);
+            }
+        }
+        while(!newUserName1.equals(END_OF_NAMES));
+
+    }
+
+}
