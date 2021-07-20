9d32e6dbbd64b99693b8b281249526e59bb22e77
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-20 23:08:46 +0200

    Basket Update BasketTest creation.

--- TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
@@ -1,5 +1,22 @@
 package WebBasket;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Basket {
 
+    Map<String, Double> orderedItem = new HashMap<>();
+    public Basket(){
+    }
+
+    public static void main(String[] args) {
+
+    Item itemID0001 = new Item("Palki perkusyjne",10.50);
+    Item itemID0002 = new Item("Stołek perkusyjny",150.65);
+    Item itemID0003 = new Item("Bijak filcowy",52.49);
+
+
+
+
+}
 }
