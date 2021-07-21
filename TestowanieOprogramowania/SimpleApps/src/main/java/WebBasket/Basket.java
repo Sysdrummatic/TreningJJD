53ec235d3bb687c66d3f3fdf665bbe3e48395e75
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-21 20:56:47 +0200

    Basket update

--- TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
@@ -5,18 +5,42 @@ import java.util.Map;
 
 public class Basket {
 
-    Map<String, Double> orderedItem = new HashMap<>();
+
     public Basket(){
+
     }
 
     public static void main(String[] args) {
 
-    Item itemID0001 = new Item("Palki perkusyjne",10.50);
-    Item itemID0002 = new Item("Stołek perkusyjny",150.65);
-    Item itemID0003 = new Item("Bijak filcowy",52.49);
+        Map<String, Double> orderedItem = new HashMap<>();
 
 
 
+
+
+
+
+
+        //Wywołanie koszyka
+
+        Basket basket = new Basket();
+
+        //Wywołanie przedmiotów
+        Item itemID0001 = new Item("Palki perkusyjne",10.50);
+        Item itemID0002 = new Item("Palki perkusyjne",10.50);
+        Item itemID0003 = new Item("Talerz Crash 18",259.50);
+        Item itemID0004 = new Item("Stołek perkusyjny",150.65);
+        Item itemID0005 = new Item("Bijak filcowy",52.49);
+        Item itemID0006 = new Item("Bijak filcowy",52.49);
+        Item itemID0007 = new Item("Stopa Twin Custom",1789.49);
+        Item itemID0008 = new Item("Stojak łamany",379.49);
+
+        //Dodanie do koszyka
+        orderedItem.put(itemID0001.getName(),itemID0001.getPrice());
+        //Basket.ordered
+
+
+        //Wyświetlenie zawartości koszyka z podsumowaniem i ceną.
 
 }
 }
