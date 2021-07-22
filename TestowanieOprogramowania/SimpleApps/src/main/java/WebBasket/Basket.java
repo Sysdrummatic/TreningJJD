fa0ce864b2483427e26b7b04d9aa261a8751d6e0
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-22 13:04:58 +0200

    RemoveAll and Buy functions creation

--- TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Basket.java
@@ -1,11 +1,15 @@
 package WebBasket;
 
+import java.sql.SQLOutput;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Basket {
 
     private final Map<Item, Integer> orderedItem = new TreeMap<>();
+
+    Scanner typePromocode = new Scanner(System.in);
 
     public void add(Item item){
         add(item,1);
@@ -23,18 +27,22 @@ public class Basket {
 
     }
 
+    public void removeAll(){
 
+    }
 
-
-
-
-
-
+    public void buy(){
+        System.out.println("Thank you for buying at DrumCo.com!");
+        System.out.println("Type a promocode if you have one. Otherwise type - and enter.");
+        String promocode = typePromocode.nextLine();
+    }
 
         //Wyświetlenie zawartości koszyka z podsumowaniem i ceną.
 
 
 public void showBasketDetails() {
+
+        //Wyświetl listę przedmiotów w koszyku z nazwą, ilością, sceną oraz zbiorczą sumą zamówienia
 
 }
 }
