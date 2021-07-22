fba38a3f2b2a1aff205e42f24bcd564c6b1c7a20
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-22 13:08:52 +0200

    BasketTest update.

--- TestowanieOprogramowania/SimpleApps/src/test/java/BasketTest.java
+++ TestowanieOprogramowania/SimpleApps/src/test/java/BasketTest.java
@@ -1,4 +1,29 @@
+import org.junit.Test;
 
 public class BasketTest {
+
+    @Test //add method tests
+    public void addTest() {
+
+
+
+    }
+    @Test
+    public void removeTest() {
+
+    }
+    @Test
+    public void removeAllTest() {
+
+    }
+    @Test
+    public void showBasketDetailsTest() {
+
+    }
+    @Test
+    public void buyTest() {
+
+    }
+
 
 }
