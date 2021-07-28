eb013f5f99deb13c56e51d4dedbec11a2167dcaf
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-28 21:56:27 +0200

    Test //given clear up

--- TestowanieOprogramowania/SimpleApps/src/test/java/ItemTest.java
+++ TestowanieOprogramowania/SimpleApps/src/test/java/ItemTest.java
@@ -6,27 +6,22 @@ import org.junit.Test;
 
 public class ItemTest {
 
-    private Item product;
     private String correctName;
     private String wrongName;
     private double doublePrice;
-    private double integerPrice;
     private double zeroPrice;
-    private double nullPrice;
     Item itemID0001;
     Item itemID0002;
     Item itemID0003;
-    Basket basket;
+
 
     @Before
     public void setUpData(){
         //given
         correctName = "Produkt 1";
         wrongName = null;
-        integerPrice = 2;
         doublePrice = 1.59;
         zeroPrice = 0;
-        product = new Item(correctName, doublePrice);
         itemID0001 = new Item("Palki perkusyjne",10.50);
         itemID0002 = new Item("Palki perkusyjne",10.50);
         itemID0003 = new Item("Talerz Crash 18",259.50);
