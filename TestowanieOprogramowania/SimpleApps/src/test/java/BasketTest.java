fdaf171d7d28e501dc035bd16bce32f05c9ffe94
Sysdrummatic <sysdrummatic@gmail.com>
2021-07-30 22:36:29 +0200

    code clear up

--- TestowanieOprogramowania/SimpleApps/src/test/java/BasketTest.java
+++ TestowanieOprogramowania/SimpleApps/src/test/java/BasketTest.java
@@ -11,21 +11,11 @@ public class BasketTest {
 
     private Basket basket;
     public Item itemID0001;
-    public Item itemID0002;
-    public Item itemID0003;
-    public Item itemID0004;
-    public Item itemID0005;
-    public Item itemID0006;
 
     @Before
     public void dataSetUp(){
 
         itemID0001 = new Item("Palki perkusyjne",10.50);
-        itemID0002 = new Item("Talerz Crash 18",259.50);
-        itemID0003 = new Item("Stołek perkusyjny",150.65);
-        itemID0004 = new Item("Bijak filcowy",52.49);
-        itemID0005 = new Item("Stopa Twin Custom",1789.49);
-        itemID0006 = new Item("Stojak łamany",379.49);
         basket = new Basket();
     }
     @Test
@@ -57,9 +47,9 @@ public class BasketTest {
     @Test
     public void shouldAddManyItemsOfTheSameType(){
 
-        basket.add(itemID0003,31);
+        basket.add(itemID0001,31);
 
-        Map<Item,Integer> expected = createOrder(itemID0003,31);
+        Map<Item,Integer> expected = createOrder(itemID0001,31);
         Assert.assertEquals(expected,basket.getOrderedItem());
     }
 
