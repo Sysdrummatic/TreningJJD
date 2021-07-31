d0b5e82dd945df608420fd9c6710e44381a50e14
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-01 01:02:51 +0200

    Main.java clear up.

--- TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Main.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/WebBasket/Main.java
@@ -3,43 +3,21 @@ package WebBasket;
 public class Main {
     public static void main(String[] args) {
 
-
         Basket basket = new Basket();
-
-        //Wywołanie przedmiotów
         Item itemID0001 = new Item("Palki perkusyjne",10.50);
-        Item itemID0002 = new Item("Palki perkusyjne",10.50);
         Item itemID0003 = new Item("Talerz Crash 18",259.50);
         Item itemID0004 = new Item("Stołek perkusyjny",150.65);
         Item itemID0005 = new Item("Bijak filcowy",52.49);
-        Item itemID0006 = new Item("Bijak filcowy",52.49);
         Item itemID0007 = new Item("Stopa Twin Custom",1789.49);
         Item itemID0008 = new Item("Stojak łamany",379.49);
 
-        //Dodanie do koszyka
+        basket.add(itemID0001,5);
+        basket.add(itemID0003,2);
+        basket.add(itemID0004,1);
+        basket.add(itemID0005,2);
+        basket.add(itemID0007,1);
+        basket.add(itemID0008,2);
 
-        System.out.println(basket.getOrderValue());
-
-        //System.out.println(basket.orderedItem.size());
-
-        basket.add(itemID0001,3);
-        basket.add(itemID0002,2);
-        basket.add(itemID0003,1);
-
-        System.out.println(basket.getOrderValue());
-
-        basket.remove(itemID0001);
-
-        System.out.println(basket.getOrderValue());
-
-        basket.add(itemID0004);
-
-        System.out.println(basket.getOrderValue());
-        System.out.println(basket.getOrderValue());
-
-        basket.showBasketDetails();
-
-        //System.out.println(basket.orderedItem.size());
-
+        basket.buy();
     }
 }
