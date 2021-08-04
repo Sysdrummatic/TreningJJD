2d2d8f2f939e05910579d789c905cf403c9c9902
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-04 21:02:55 +0200

    PostOffice.java finish

--- WzorceProjektowe/PierwszeSpotkanie/src/main/java/Classes/PostOffice.java
+++ WzorceProjektowe/PierwszeSpotkanie/src/main/java/Classes/PostOffice.java
@@ -1,4 +1,11 @@
 package Classes;
 
-public class PostOffice {
+import Interfaces.DeliveryService;
+
+public class PostOffice implements DeliveryService {
+
+    @Override
+    public void delivery(Parcel parcel) {
+        System.out.println(String.format("Parcel %s was delivered by PostOffice", parcel));
+    }
 }
