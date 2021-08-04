2f83c46832da4b21f7972476018da49f2ebd7490
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-04 21:20:13 +0200

    Main creation

--- WzorceProjektowe/PierwszeSpotkanie/src/main/java/Main.java
+++ WzorceProjektowe/PierwszeSpotkanie/src/main/java/Main.java
@@ -1,2 +1,14 @@
+import Classes.Courier;
+import Classes.OrderLifecycle;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        OrderLifecycle postOfficeOrder = new OrderLifecycle();
+
+
+    }
+
 }
