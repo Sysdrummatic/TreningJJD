0e0060285d2e8e60182210042e6ea02cdc733b36
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-04 20:59:23 +0200

    Parcel.java finish

--- WzorceProjektowe/PierwszeSpotkanie/src/main/java/Classes/Parcel.java
+++ WzorceProjektowe/PierwszeSpotkanie/src/main/java/Classes/Parcel.java
@@ -1,4 +1,16 @@
 package Classes;
 
 public class Parcel {
+
+    private String orderId;
+
+    public Parcel(String orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[samro:%s]", orderId);
+    }
 }
+
