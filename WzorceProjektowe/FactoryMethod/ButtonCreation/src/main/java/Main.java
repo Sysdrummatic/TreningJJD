5fd8e98c89acf0c305dbd78abecb6664e04a4d85
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-07 23:04:17 +0200

    Main creation

--- WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/Main.java
+++ WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/Main.java
@@ -1,2 +1,27 @@
+import ConcreteCreator.HtmlDialog;
+import ConcreteCreator.WindowsDialog;
+import Creator.Dialog;
+
 public class Main {
+
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+
+        configure();
+        runBussinesLogic();
+
+    }
+    static void configure(){
+        if (System.getProperty("os.name").equals("Windows 190")){
+            dialog = new WindowsDialog();
+
+        }else{
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBussinesLogic() {
+        dialog.renderWindow();
+    }
 }
