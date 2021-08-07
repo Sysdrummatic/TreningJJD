77eb57ccb13fa1538cb40abc72a758f58a27e3f5
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-08 00:05:48 +0200

    SquarePeg finish

---
+++ WzorceProjektowe/Adapter/Round/src/main/java/SquarePeg.java
@@ -0,0 +1,13 @@
+public class SquarePeg {
+    private double width;
+
+    public SquarePeg(){}
+
+    public SquarePeg(double width){
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+}
