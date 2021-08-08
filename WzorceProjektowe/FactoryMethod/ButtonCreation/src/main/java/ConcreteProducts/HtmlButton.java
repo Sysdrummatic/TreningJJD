4f1e3b7a115c5872b6d2ce8aa1b3d7df9c2ee165
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-08 22:14:02 +0200

    HtTMLButton modification label parameter add to render() method

--- WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/ConcreteProducts/HtmlButton.java
+++ WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/ConcreteProducts/HtmlButton.java
@@ -3,10 +3,11 @@ package ConcreteProducts;
 import Product.Button;
 
 public class HtmlButton implements Button {
-
+    public String label = "<button>Test Button</button>";
     @Override
-    public void render() {
-        System.out.println("<button>Test Button</button>");
+    public void render(String label) {
+        this.label = label;
+        System.out.println(label);
         onClick();
     }
 
