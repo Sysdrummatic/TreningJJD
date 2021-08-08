034721838438eae6d49be1c2d33c52e5e35c2081
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-08 21:03:39 +0200

    WindowsHellButton creation

---
+++ WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/ConcreteProducts/WindowsHellButton.java
@@ -0,0 +1,20 @@
+package ConcreteProducts;
+
+import Product.Button;
+
+public class WindowsHellButton implements Button {
+
+    public WindowsHellButton(){
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void onClick() {
+
+    }
+}
