1116722fa8b2b0aa1cc92049aa26c0be0372b7e1
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-09 00:05:56 +0200

    WindowsHellButton correction

--- WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/ConcreteProducts/WindowsHellButton.java
+++ WzorceProjektowe/FactoryMethod/ButtonCreation/src/main/java/ConcreteProducts/WindowsHellButton.java
@@ -2,19 +2,22 @@ package ConcreteProducts;
 
 import Product.Button;
 
+import javax.swing.*;
+
 public class WindowsHellButton extends WindowsButton implements Button {
-
-    public WindowsHellButton(){
-
-    }
 
     @Override
     public void render() {
-
+        super.render();
     }
 
     @Override
     public void onClick() {
+        button = new JButton("Go to hell");
+        button.addActionListener(e -> {
+            frame.setVisible(false);
+            System.exit(0);
+        });
 
     }
 }
