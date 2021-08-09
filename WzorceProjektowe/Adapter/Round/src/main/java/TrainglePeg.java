cf61bf6ac03f18c2b0ec4e4aed82eb3804599b88
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-09 19:07:57 +0200

    TrianglePeg finish

---
+++ WzorceProjektowe/Adapter/Round/src/main/java/TrainglePeg.java
@@ -0,0 +1,19 @@
+class TrianglePeg {
+private double height;
+private double base;
+
+    public TrianglePeg(){}
+
+    public TrianglePeg(double height, double base){
+        this.height = height;
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+}
