09dd6b2b70951cd6ad6f626289c512b336178566
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-10 21:40:09 +0200

    CompressionDecorator update

--- WzorceProjektowe/Decorator/src/main/java/CompressionDecorator.java
+++ WzorceProjektowe/Decorator/src/main/java/CompressionDecorator.java
@@ -1,7 +1,28 @@
 public class CompressionDecorator extends DataSourceDecorator{
-    private int complevel = 6;
+    private int compressionLevel = 6;
 
     public CompressionDecorator(DataSource source) {
         super(source);
     }
+
+    public int getCompressionLevel() {
+        return compressionLevel;
+    }
+
+
+    public void setCompressionLevel(int value) {
+        this.compressionLevel = value;
+    }
+
+    @Override
+    public void writeData(String data){
+        super.writeData(compress(data));
+    }
+
+    @Override
+    public String readData(String data){
+        return decompress(super.readData());
+    }
+
+
 }
