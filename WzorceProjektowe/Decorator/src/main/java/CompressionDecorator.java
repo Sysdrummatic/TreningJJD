1d3548e53952955b04c755043e29645b805b2eff
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-10 22:16:06 +0200

    CompressionDecorator finish

--- WzorceProjektowe/Decorator/src/main/java/CompressionDecorator.java
+++ WzorceProjektowe/Decorator/src/main/java/CompressionDecorator.java
@@ -1,3 +1,12 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+
 public class CompressionDecorator extends DataSourceDecorator{
     private int compressionLevel = 6;
 
@@ -20,9 +29,41 @@ public class CompressionDecorator extends DataSourceDecorator{
     }
 
     @Override
-    public String readData(String data){
+    public String readData(){
         return decompress(super.readData());
     }
 
+    private String compress(String stringData){
+        byte[] data = stringData.getBytes();
+        try{
+            ByteArrayOutputStream bout = new ByteArrayOutputStream(512);
+            DeflaterOutputStream dos = new DeflaterOutputStream(bout, new Deflater(compressionLevel));
+            dos.write(data);
+            dos.close();
+            bout.close();
+            return Base64.getEncoder().encodeToString(bout.toByteArray());
+        } catch (IOException ex) {
+            return null;
+        }
+    }
 
+    private String decompress(String stringData) {
+        byte[] data = Base64.getDecoder().decode(stringData);
+        try {
+            InputStream in = new ByteArrayInputStream(data);
+            InflaterInputStream iin = new InflaterInputStream(in);
+            ByteArrayOutputStream bout = new ByteArrayOutputStream(512);
+            int b;
+            while((b = iin.read()) != -1){
+                bout.write(b);
+            }
+            in.close();
+            iin.close();
+            bout.close();
+            return new String(bout.toByteArray());
+        } catch (IOException ex) {
+            return null;
+        }
+
+    }
 }
