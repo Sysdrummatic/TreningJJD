8176dc719344b4496515a3b1bfeccd267ae24ae3
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-11 22:59:53 +0200

    PizzaWithMozzarella creation

---
+++ WzorceProjektowe/Decorator/Pizza/src/main/java/PizzaWithMozzarella.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+public class PizzaWithMozzarella extends Pizza {
+    private static final BigDecimal MOZZARELLA_PRICE = new BigDecimal(5);
+    private Pizza basePizza;
+
+    public PizzaWithMozzarella(Pizza basePizza){
+        this.basePizza = basePizza;
+    }
+
+}
