fa4595ea26aab6e8df311d07ae6675122fb90ba8
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-11 23:40:22 +0200

    Main modification

--- WzorceProjektowe/Decorator/Pizza/src/main/java/Main.java
+++ WzorceProjektowe/Decorator/Pizza/src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.text.DecimalFormat;
+import java.util.List;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        Pizza margherita = new Pizza();
+        Pizza withMozzarella = new PizzaWithMozzarella(margherita);
+        Pizza withHam = new PizzaWithHam(withMozzarella);
+
+
+    DecimalFormat df = new DecimalFormat("#,00 zł");
+    for(Pizza pizza : List.of (margherita,withMozzarella,withHam)){
+        System.out.println(String.format("%s costs %s.", pizza, df.format(pizza.getPrice())));
+    }
+}}
