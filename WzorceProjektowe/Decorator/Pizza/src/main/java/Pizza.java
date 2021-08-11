8bdc554baa86ab34ac46bc6d70ed3c3057a2e1ae
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-11 22:55:08 +0200

    Pizza finish

---
+++ WzorceProjektowe/Decorator/Pizza/src/main/java/Pizza.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class Pizza {
+
+        private static final BigDecimal BASE_PRICE = new BigDecimal(12);
+
+        public BigDecimal getPrice() {
+            return BASE_PRICE;
+        }
+
+        public String toString() {
+            return "Pizza";
+        }
+}
