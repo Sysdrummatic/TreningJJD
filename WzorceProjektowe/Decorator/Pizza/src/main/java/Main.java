a9bb8e82a1c025764b49907dba500d7ce082dd6b
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-12 20:47:28 +0200

    Main update

--- WzorceProjektowe/Decorator/Pizza/src/main/java/Main.java
+++ WzorceProjektowe/Decorator/Pizza/src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Pizza withMozzarella = new PizzaWithMozzarella(margherita);
         Pizza withHam = new PizzaWithHam(withMozzarella);
 
-
     DecimalFormat df = new DecimalFormat("#,00 zł");
     for(Pizza pizza : List.of (margherita,withMozzarella,withHam)){
         System.out.println(String.format("%s costs %s.", pizza, df.format(pizza.getPrice())));
