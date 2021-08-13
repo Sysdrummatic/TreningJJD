5c6b3f018da9ad977e38bb86841fbca412c0b47f
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-13 20:56:17 +0200

    PizzaWithBasil finish

--- WzorceProjektowe/Decorator/Pizza/src/main/java/PizzaWithBasil.java
+++ WzorceProjektowe/Decorator/Pizza/src/main/java/PizzaWithBasil.java
@@ -1,2 +1,9 @@
-public class PizzaWithBasil {
+import java.math.BigDecimal;
+
+public class PizzaWithBasil extends PizzaWithTopping{
+    private static final BigDecimal BASIL_PRICE = new BigDecimal(3);
+
+    public PizzaWithBasil(Pizza basePrice) {
+        super(basePrice, BASIL_PRICE);
+    }
 }
