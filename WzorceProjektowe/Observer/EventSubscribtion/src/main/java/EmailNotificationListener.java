024223ae27d1220940f716c926949f99172ed2f7
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-15 01:16:05 +0200

    EmailNotificationListener finish.

--- WzorceProjektowe/Observer/EventSubscribtion/src/main/java/EmailNotificationListener.java
+++ WzorceProjektowe/Observer/EventSubscribtion/src/main/java/EmailNotificationListener.java
@@ -1,8 +1,15 @@
 import java.io.File;
 
 public class EmailNotificationListener implements EventListener {
+    private String mail;
+
+    public EmailNotificationListener(String mail) {
+        this.mail = mail;
+    }
+
     @Override
     public void update(String eventType, File file) {
+        System.out.println("Email to " + mail + ": Someone has performed " + eventType + " operation with the following file: " + file.getName());
 
     }
 }
