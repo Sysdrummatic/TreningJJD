1dc4b442fa751f39d8ce1046f8c3fc577138046a
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-14 23:40:44 +0200

    EventManager creation

---
+++ WzorceProjektowe/Observer/EventSubscribtion/src/main/java/EventManager.java
@@ -0,0 +1,8 @@
+import java.util.EventListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EventManager {
+    Map<String, List<EventListener>> listeners = new HashMap<>();
+}
