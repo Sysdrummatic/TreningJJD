2966752c3d0c86261e19b52ade0db0a7f449b6b0
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-15 01:27:54 +0200

    Main finish.

--- WzorceProjektowe/Observer/EventSubscribtion/src/main/java/Main.java
+++ WzorceProjektowe/Observer/EventSubscribtion/src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("sysdrummatic@gmail.com"));
+
+        try {
+            editor.openFile("text.txt");
+            editor.saveFile();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
