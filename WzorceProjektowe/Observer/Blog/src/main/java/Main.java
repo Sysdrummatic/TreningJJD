4601cdda168a4fd1932903649fc8ae6887a640e7
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-15 22:39:09 +0200

    Main fnish

--- WzorceProjektowe/Observer/Blog/src/main/java/Main.java
+++ WzorceProjektowe/Observer/Blog/src/main/java/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        Blog lukaszmichtaPL = new Blog();
+        Reader webObserver = new Reader(lukaszmichtaPL);
+        lukaszmichtaPL.subscribe(webObserver);
+        lukaszmichtaPL.publishArticle("Mega NewArticle1");
+        lukaszmichtaPL.publishArticle("Mega NewArticle2");
+        lukaszmichtaPL.unsubscribe(webObserver);
+        lukaszmichtaPL.publishArticle("Mega NewArticle3");
+
+    }
 }
