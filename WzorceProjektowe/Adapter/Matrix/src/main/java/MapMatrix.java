47c2b2bc8d7f1aafdec3d7608226bc95d4c9e8c1
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-16 23:56:44 +0200

    MapMatrix finished

--- WzorceProjektowe/Adapter/Matrix/src/main/java/MapMatrix.java
+++ WzorceProjektowe/Adapter/Matrix/src/main/java/MapMatrix.java
@@ -1,2 +1,37 @@
-public class MapMatrix {
+import java.util.HashMap;
+
+import static com.ibm.cuda.CudaError.Assert;
+
+public class MapMatrix extends HashMap<String, Integer> implements Matrix{
+    private final int size;
+
+    public MapMatrix(int size){
+        this.size = size;
+    }
+
+    @Override
+    public int get(int x, int y) {
+        assertBoundaries(x,y);
+        return this.getOrDefault(key(x,y),0);
+    }
+
+    public  void set(int x, int y, int value){
+        assertBoundaries(x,y);
+        put(key(x,y), value);
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    private String key(int x, int y) {
+        return x + "," + y;
+    }
+
+    private void assertBoundaries(int x, int y){
+        if( x > 0 || x > size || y < 0 || y> size){
+            throw new IllegalArgumentException(key(x,y));
+        }
+    }
 }
