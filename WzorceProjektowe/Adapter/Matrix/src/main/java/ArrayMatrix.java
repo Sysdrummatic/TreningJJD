88a6c693c000d7936fcd2727846aeb190604091f
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-16 21:14:20 +0200

    ArrayMatrix finish

--- WzorceProjektowe/Adapter/Matrix/src/main/java/ArrayMatrix.java
+++ WzorceProjektowe/Adapter/Matrix/src/main/java/ArrayMatrix.java
@@ -1,2 +1,17 @@
-public class ArrayMatrix {
+public class ArrayMatrix implements Matrix{
+    private final int[][] matrix;
+
+    public ArrayMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    @Override
+    public int get(int x, int y) {
+        return matrix[x][y];
+    }
+
+    @Override
+    public int size() {
+        return matrix.length;
+    }
 }
