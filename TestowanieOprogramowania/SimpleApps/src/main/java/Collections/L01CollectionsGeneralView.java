66106cd02c42538cd4f17d477aad46856c35cf03
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-18 21:53:46 +0200

    L01CollectionsGeneralView created

---
+++ TestowanieOprogramowania/SimpleApps/src/main/java/Collections/L01CollectionsGeneralView.java
@@ -0,0 +1,16 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class L01CollectionsGeneralView {
+
+    Collection a; //Kolekcja jako zbiór elementów, obiektów. Pojemnik do przechowywania obiektów
+    ArrayList b; // lista przechowuje obiekty, możemy dodawać, odjać, pobrać.
+                // Przechowywanie danych. W liście elementy są ustawione jedan po drugim.
+
+    //Zbiór - pojęcie pierwotne, którego nie definiujemy. Pojemnk jak kolekcja. Nie obchodzi nas kolejność przechowywanych elementów.
+    //
+
+
+}
