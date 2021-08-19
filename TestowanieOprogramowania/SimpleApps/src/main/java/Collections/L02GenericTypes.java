26d19a4ac2405dfedb0f2dfb0af031f8a008114d
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-19 22:55:39 +0200

    Generic types first attempt.

--- TestowanieOprogramowania/SimpleApps/src/main/java/Collections/L02GenericTypes.java
+++ TestowanieOprogramowania/SimpleApps/src/main/java/Collections/L02GenericTypes.java
@@ -1,4 +1,36 @@
 package Collections;
 
+import java.util.ArrayList;
+
 public class L02GenericTypes {
+
+    public static void main(String[] args) {
+/*
+        ArrayList a = new ArrayList();
+
+        a.add(new String("lala"));
+        a.add(new Integer(4));
+
+        for (int i = 0; i < a.size(); i++)
+            System.out.println((String)a.get(i));
+*/
+        Zwykla<String> nazwaZmiennej = new Zwykla();
+        nazwaZmiennej.nadajWartość("To jest tekst tekstowy");
+        System.out.println(nazwaZmiennej.gveItBack());
+    }
+}
+
+class Zwykla <COKOLWIEK>
+{
+    COKOLWIEK nazwaZmiennejTypuCokolwiek;
+
+    public  void nadajWartość (COKOLWIEK wartosc)
+    {
+        this.nazwaZmiennejTypuCokolwiek = wartosc;
+    }
+
+    public COKOLWIEK gveItBack()
+    {
+        return this.nazwaZmiennejTypuCokolwiek;
+    }
 }
