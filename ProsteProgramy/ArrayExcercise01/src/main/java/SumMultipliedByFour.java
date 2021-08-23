6bee430323214b4a4698e90c7ff4cfa6c7af539a
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-23 23:31:33 +0200

    Correction

--- ProsteProgramy/ArrayExcercise01/src/main/java/SumMultipliedByFour.java
+++ ProsteProgramy/ArrayExcercise01/src/main/java/SumMultipliedByFour.java
@@ -2,10 +2,11 @@ public class SumMultipliedByFour {
 
     public int solution(int[] A) {
     int sum = 0;
-        //for (int a : A)
+
+    //for (int a : A)
         for(int a = 0; a < A.length; a++ ) // sprwdzić jak napisać to w formie zwykłej pętli jakby ktoś pytal ;)
         {
-        if (a % 4 == 0) {
+            if (a % 4 == 0) {
             sum += a;
         }
     }
