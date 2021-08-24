79b34b46aab8a1734a4499ff0526e234ca10aa2d
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-24 22:47:13 +0200

    Project start

---
+++ ProsteProgramy/TransformCSVtoObject/src/main/java/TransformCSVtoObject.java
@@ -0,0 +1,32 @@
+public class TransformCSVtoObject {
+
+    public static void main(String[] args) {
+
+    }
+
+    Address parseAddress(String addressAsString){
+
+        return new Address();
+    }
+
+
+
+}
+
+
+//"Rīga, Āraišu iela 36 - 1A"
+
+/*
+Address parseAddress(String addressAsString);
+
+I powinna działać tak
+
+Address address = parseAddress("Rīga, Āraišu iela 36 - 1A")
+address.getCity() == "Riga"
+address.getStreet() == "Āraišu iela"
+address.getHouseNumber() == "36 - 1A"
+
+
+
+
+ */
