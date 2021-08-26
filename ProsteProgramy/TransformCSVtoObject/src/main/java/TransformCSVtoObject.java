280ad789bd7fea39fec16f417c4a53eb51b27052
Sysdrummatic <sysdrummatic@gmail.com>
2021-08-26 23:58:24 +0200

    TransformCSVtoObject update.

--- ProsteProgramy/TransformCSVtoObject/src/main/java/TransformCSVtoObject.java
+++ ProsteProgramy/TransformCSVtoObject/src/main/java/TransformCSVtoObject.java
@@ -2,21 +2,25 @@ public class TransformCSVtoObject {
 
     public static void main(String[] args) {
 
-       // Address address = parseAddress("Rīga, Āraišu iela 36 - 1A")
+       Address address = parseAddress("Rīga, Āraišu iela 36 - 1A");
+
+
 
 
     }
 
+     static Address parseAddress(String addressAsString){
 
+        String[] parts = addressAsString.split(",");
 
+        String administrativeArea = parts[0];
+        String city = parts[1];
+        String street = parts[2];
+        String houseNumber = parts[3];
+        String houseName = parts[4];
 
-    //Address parseAddress(String addressAsString){
-
-    //    return new Address();
-    //}
-
-
-
+        return new Address(administrativeArea, city, street, houseNumber, houseName);
+    }
 }
 
 
