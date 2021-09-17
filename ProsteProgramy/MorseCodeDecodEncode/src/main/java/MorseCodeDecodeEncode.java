84b284cbcfb99dc5d8bd2b88bcac4ca7c3be5698
Sysdrummatic <sysdrummatic@gmail.com>
2021-09-18 00:56:41 +0200

    Encode method finished.

--- ProsteProgramy/MorseCodeDecodEncode/src/main/java/MorseCodeDecodeEncode.java
+++ ProsteProgramy/MorseCodeDecodEncode/src/main/java/MorseCodeDecodeEncode.java
@@ -5,25 +5,49 @@ public class MorseCodeDecodeEncode {
         String morseCodeToTranslate = morseCode;
         StringBuilder textAfterTranslation = new StringBuilder();
 
-
-
         morseCodeToTranslate = morseCodeToTranslate.toLowerCase();
+
+
+        char[] letters = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        String[] morseLetters = { "    ", ". ___", "___ . . .", "___ . ___ .", "___ . .", ".", ". . ___ .", "___ ___ .", ". . . .", ". .", ". ___ ___ ___", "___ . ___", ". ___ . .",  "___ ___", "___ .", "___ ___ ___", ". ___ ___ .", "___ ___ . ___", ". ___ .", ". . .", "_", ". . ___", ". . . ___", ". ___ ___", "___ . . ___", "___ . ___ ___", "___ ___ . .", ". ___ ___ ___ ___", ". . ___ ___ ___", ". . . ___ ___", ". . . . ___", ". . . . .", "___ . . . .", "___ ___ . . .", "___ ___ ___ . .", "___ ___ ___ ___ .", "___ ___ ___ ___ ___"};
+
 
         for (int i = 0; i < morseCodeToTranslate.length(); i++) {
             for (short j = 0; j < 37; j++) {
-                if (morseCodeToTranslate.charAt(i) == AlfaMorseTables.letters[j]) {
-                    textAfterTranslation.append(AlfaMorseTables.morseLetters[j]);
+                if (morseCodeToTranslate.charAt(i) == letters[j]) {
+                    textAfterTranslation.append(morseLetters[j]);
                     textAfterTranslation.append("   ");
 
-                    break;
+
                 }
             }
         }
         return textAfterTranslation.toString();
     }
 
-    public static String encode(String morseCode){
+    public static String encode(String textToEncode){
 
-        return "";
+        StringBuilder textAfterTranslation = new StringBuilder();
+        textToEncode = textToEncode.toLowerCase();
+
+        char[] letters = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        String[] morseLetters = { " ", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",  "--", "-.", "---", ".--.", "--.-", ".-.", "...", "_", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
+
+        for (int i = 0; i < textToEncode.length(); i++)
+        {
+            for (short j = 0; j < 37; j++)
+            {
+                if (textToEncode.charAt(i) == letters[j])
+                {
+                    textAfterTranslation.append(morseLetters[j]);
+                    if (i != textToEncode.length()-1)
+                    {
+                        textAfterTranslation.append(" ");
+                    }
+                    break;
+                }
+            }
+        }
+        return textAfterTranslation.toString();
     }
 }
