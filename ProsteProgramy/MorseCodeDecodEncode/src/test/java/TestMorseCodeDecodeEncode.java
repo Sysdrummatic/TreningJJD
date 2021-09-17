18e3b48b722f7b90e774957e96b204664fd883d5
Sysdrummatic <sysdrummatic@gmail.com>
2021-09-18 00:56:07 +0200

    Test modification.

--- ProsteProgramy/MorseCodeDecodEncode/src/test/java/TestMorseCodeDecodeEncode.java
+++ ProsteProgramy/MorseCodeDecodEncode/src/test/java/TestMorseCodeDecodeEncode.java
@@ -1,7 +1,15 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMorseCodeDecodeEncode {
+
+/*    @Before
+//    public void dataSetup() {
+//        String morseCodeToTranslate = ".... . -.--   .--- ..- -.. .";
+
+    "HEY JUDE"
+*/
 
     @Test
     public void testIfMessageIsDecodedCorrect() {
@@ -10,11 +18,24 @@ public class TestMorseCodeDecodeEncode {
 
     @Test
     public void testIfMessageIsEncodedCorrect() {
-        Assert.assertEquals(MorseCodeDecodeEncode.encode("HEY JUDE"), (".... . -.--   .--- ..- -.. ."));
+
+        Assert.assertEquals(".... . -.--   .--- ..- -.. .", MorseCodeDecodeEncode.encode("HEY JUDE"));
     }
 
     @Test
     public void testIfDataFromTablesCanBeReached() {
         Assert.assertEquals('a',AlfaMorseTables.letters[1]);
+    }
+
+    @Test
+    public void testTheLengthOfTheString() {
+        String morseCodeToTranslate = ".... . -.--   .--- ..- -.. .";
+        Assert.assertEquals(28,morseCodeToTranslate.length());
+    }
+
+    @Test
+    public void testTheLengthOfTheCharTable() {
+
+        Assert.assertEquals(37, AlfaMorseTables.letters.length);
     }
 }
