5b45f46444b518a98bfdbac9fee936f41b7ff210
Sysdrummatic <sysdrummatic@gmail.com>
2021-09-18 23:10:25 +0200

    Additional support test added.

--- ProsteProgramy/MorseCodeDecodEncode/src/test/java/TestMorseCodeDecodeEncode.java
+++ ProsteProgramy/MorseCodeDecodEncode/src/test/java/TestMorseCodeDecodeEncode.java
@@ -1,17 +1,30 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public class TestMorseCodeDecodeEncode {
 
     @Test
     public void testIfMessageIsDecodedCorrect() {
-        Assert.assertEquals("hej jude",MorseCodeDecodeEncode.decode(".... . -.--   .--- ..- -.. ."));
+        Assert.assertEquals("hey jude",MorseCodeDecodeEncode.decode(".... . -.--   .--- ..- -.. ."));
     }
 
     @Test
     public void testIfMessageIsEncodedCorrect() {
 
         Assert.assertEquals(".... . -.--   .--- ..- -.. .", MorseCodeDecodeEncode.encode("HEY JUDE"));
+    }
+
+    @Test
+    public void testIfListIsOK() {
+        String morseCode = ".... . -.--   .--- ..- -.. .";
+        List<String> splittedMorseCode = Arrays.asList(morseCode.split(" "));
+
+
+        Assert.assertEquals(" ", splittedMorseCode.get(3));
     }
 
 }
